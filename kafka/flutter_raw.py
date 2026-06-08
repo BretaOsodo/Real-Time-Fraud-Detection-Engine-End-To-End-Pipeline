280fb90f0b617faf016b90f5dac16f9d1725eb93
Breta Osod <bretaosodo@gmail.com>
2026-06-08 21:27:04 +0300

    Packaged the data generator file

--- kafka/flutter_raw.py
+++ kafka/flutter_raw.py
@@ -4,3 +4,37 @@ import logging
 import json
 
 logging.basicConfig(level=logging.INFO)
+logger= logging.getLogger(__name__)
+
+#A function to produce the flutter wave transaction
+def produce_flutter_transaction():
+    producer_config={
+        'bootstrap.servers':'localhost:9092',
+        'acks':'all',
+        'retries':10,
+        'enable.idempontence':True
+
+    }
+
+    producer= Producer(producer_config)
+    generator=TransactionDataGenerator()
+    count = 0
+
+    while count < 100000:
+        flutter_data=generator.generate_flutterwave_transaction()
+
+        producer.produce(
+            topic='flutter_raw',
+            value=json.dumps(flutter_data).encode('utf-8')
+        )
+
+        count +=1
+        producer.poll(0) #serve delivery callbacks without blocking
+        print(f'Record [{count}]: {flutter_data}')
+        logger.info(f'Successfully Produced record [{count}]')
+
+    producer.flush()
+    logger.info('All records produced successfully')
+
+if __name__=='__main__':
+    produce_flutter_transaction()
